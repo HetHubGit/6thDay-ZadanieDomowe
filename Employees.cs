e41358d4165edeb8a140993655b212bbf777b6db
HetHubGit <164913849+HetHubGit@users.noreply.github.com>
2024-05-26 12:15:58 +0100

    Add project files.

---
+++ Employees.cs
@@ -0,0 +1,35 @@
+﻿namespace _6thDay_ZadanieDomowe;
+
+internal class Employee
+{
+    private List<int> score = new List<int>();
+        public string name { get; private set; }
+        public string surname { get; private set; }
+        public int age { get; private set; }
+        public Employee(string name, string surname, int age)
+        {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        }
+    /*
+        
+    */
+
+    public int Result
+    {
+        get
+        {
+            return score.Sum();
+        }
+    }
+    public void AddScore(int number)
+    { 
+        score.Add(number);
+    }
+
+
+ }
+
+
+
